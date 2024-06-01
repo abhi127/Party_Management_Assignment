e55ef8f61a539e26e051822e52457b5a4ada80dd
abhishek.yadav-at-706124561033 <abhishek.yadav@ehspl.com>
2024-06-01 18:56:04 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListComponent } from './dashboard/list/list.component';
+import { AddPartyComponent } from './dashboard/add-party/add-party.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',component:LoginComponent,
+  },
+  {
+    path:'dashboard',canActivate:[AuthGuard],component:DashboardComponent,
+    children:[
+      {path:'',component:ListComponent},
+      {path:'add',component:AddPartyComponent},
+      {path:'edit/:id',component:AddPartyComponent}
+    ]
+  },
+  {
+    path:'**',pathMatch:'full',redirectTo:'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
